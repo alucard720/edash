26baffde0af842dba2a22450a29ddf00400c0c20
alucard720 <ing.santana36@gmail.com>
2023-04-28 22:40:18 -0400

    pc1.0

---
+++ server/db/config.js
@@ -0,0 +1,2 @@
+ const mongoose = require('mongoose')
+ mongoose.connect("mongodb+srv://ingsantana36:cCXaJpIpUzwAyYVa@cluster0.ac1os9s.mongodb.net/?retryWrites=true&w=majority")
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const app = express();
 
-app.get('/',(req, res)=>{
-    res.send('hello world')
+require("./db/config")
+require('dotenv').config()
+const User = require("./db/User")
+const app = express();
+app.use(express.json());
+
+app.post("/register", async(req, res)=>{
+    let user = new User(req.body)
+    let result = await user.save()
+    res.send(req.body)
 })
 
-app.listen(3500);
+const port = process.env.PORT
+
+app.listen(port, ()=>console.log(`Server is running in ${port}`));
